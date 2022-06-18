83721a0b451e1ce9540d755ca543b42f42149fa6
imron <imron.kyu2@gmail.com>
2022-06-18 23:58:30 +0700

    view and viewgroup

---
+++ app/src/main/java/com/example/belajarandroidkotlin/viewandviewgroup/MyViewAndViewsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.belajarandroidkotlin.viewandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.belajarandroidkotlin.R
+
+class MyViewAndViewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_view_and_views)
+    }
+}
